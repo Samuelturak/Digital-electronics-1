007e191519cf3e73191229159b3a673fd31596f9
Samuelturak <xturak01@vutbr.cz>
2021-03-06 15:30:48 +0100

    changed

---
+++ Labs/04-segment/display/display.srcs/sim_1/new/tb_hex_7seg.vhd
@@ -0,0 +1,64 @@
+------------------------------------------------------------------------
+--
+-- Testbench for 2-bit binary comparator.
+-- EDA Playground
+--
+-- Copyright (c) 2020-2021 Tomas Fryza
+-- Dept. of Radio Electronics, Brno University of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+------------------------------------------------------------------------
+-- Entity declaration for testbench
+------------------------------------------------------------------------
+entity tb_hex_7seg is
+    -- Entity of testbench is always empty
+end entity tb_hex_7seg;
+
+------------------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------------------
+architecture testbench of tb_hex_7seg is
+
+    -- Local signals
+    signal s_hex       : std_logic_vector(4 - 1 downto 0);
+    signal s_seg       : std_logic_vector(7 - 1 downto 0);
+
+begin
+    -- Connecting testbench signals with mux_2bit_4to1 entity (Unit Under Test)
+    uut_hex_7seg : entity work.hex_7seg
+        port map(
+            hex_i => s_hex,
+            seg_o => s_seg
+        );
+
+
+    --------------------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------------------
+    p_stimulus : process
+    begin
+        -- Report a note at the begining of stimulus process
+        report "Stimulus process started" severity note;
+        
+        
+         s_hex <= "0000"; wait for 100ns;
+         
+         s_hex <= "0001"; wait for 100ns;
+         
+         s_hex <= "0010"; wait for 100ns;
+         
+         s_hex <= "0011"; wait for 100ns;
+         
+        
+        
+        -- Report a note at the end of stimulus process
+        report "Stimulus process finished" severity note;
+        wait;
+    end process p_stimulus;
+
+end architecture testbench;
