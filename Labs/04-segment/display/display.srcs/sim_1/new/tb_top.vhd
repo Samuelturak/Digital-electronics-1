9980828d48a3cb41ee379da0b4d8c4a8c9d83a58
Samuelturak <xturak01@vutbr.cz>
2021-03-09 10:05:59 +0100

    changed

--- Labs/04-segment/display/display.srcs/sim_1/new/tb_top.vhd
+++ Labs/04-segment/display/display.srcs/sim_1/new/tb_top.vhd
@@ -72,22 +72,22 @@ p_stimulus : process
     begin
         
         report "Stimulus process started" severity note;
-        s_SW <= "0000";  wait for 10 ns;        
-        s_SW <= "0001";  wait for 10 ns;
-        s_SW <= "0010";  wait for 10 ns;
-        s_SW <= "0011";  wait for 10 ns;
-        s_SW <= "0100";  wait for 10 ns;
-        s_SW <= "0101";  wait for 10 ns;
-        s_SW <= "0110";  wait for 10 ns;
-        s_SW <= "0111";  wait for 10 ns;
-        s_SW <= "1000";  wait for 10 ns;
-        s_SW <= "1001";  wait for 10 ns;
-        s_SW <= "1010";  wait for 10 ns;
-        s_SW <= "1011";  wait for 10 ns;
-        s_SW <= "1100";  wait for 10 ns;
-        s_SW <= "1101";  wait for 10 ns;
-        s_SW <= "1110";  wait for 10 ns;
-        s_SW <= "1111";  wait for 10 ns;        
+        s_SW <= "0000";  wait for 50 ns;        
+        s_SW <= "0001";  wait for 50 ns;
+        s_SW <= "0010";  wait for 50 ns;
+        s_SW <= "0011";  wait for 50 ns;
+        s_SW <= "0100";  wait for 50 ns;
+        s_SW <= "0101";  wait for 50 ns;
+        s_SW <= "0110";  wait for 50 ns;
+        s_SW <= "0111";  wait for 50 ns;
+        s_SW <= "1000";  wait for 50 ns;
+        s_SW <= "1001";  wait for 50 ns;
+        s_SW <= "1010";  wait for 50 ns;
+        s_SW <= "1011";  wait for 50 ns;
+        s_SW <= "1100";  wait for 50 ns;
+        s_SW <= "1101";  wait for 50 ns;
+        s_SW <= "1110";  wait for 50 ns;
+        s_SW <= "1111";  wait for 50 ns;        
                      
         -- Report a note at the end of stimulus process
         report "Stimulus process finished" severity note;
