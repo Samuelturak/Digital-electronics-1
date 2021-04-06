1dd23f0bbe02aa471b6e68292a13edc9bc7093e5
Samuelturak <xturak01@vutbr.cz>
2021-04-06 18:55:09 +0200

    changed

--- Labs/08-traffic_lights/traffic/traffic.srcs/sources_1/new/smart_tlc.vhd
+++ Labs/08-traffic_lights/traffic/traffic.srcs/sources_1/new/smart_tlc.vhd
@@ -1,30 +1,86 @@
-# 1. Preperation Task
+------------------------------------------------------------------------
+--
+-- Traffic light controller using FSM.
+-- Nexys A7-50T, Vivado v2020.1.1, EDA Playground
+--
+-- Copyright (c) 2020-Present Tomas Fryza
+-- Dept. of Radio Electronics, Brno University of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+-- This code is inspired by:
+-- [1] LBEbooks, Lesson 92 - Example 62: Traffic Light Controller
+--     https://www.youtube.com/watch?v=6_Rotnw1hFM
+-- [2] David Williams, Implementing a Finite State Machine in VHDL
+--     https://www.allaboutcircuits.com/technical-articles/implementing-a-finite-state-machine-in-vhdl/
+-- [3] VHDLwhiz, One-process vs two-process vs three-process state machine
+--     https://vhdlwhiz.com/n-process-state-machine/
+--
+------------------------------------------------------------------------
 
-## State table:
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-| **Input P** | **0** | **0** | **1** | **1** | **0** | **1** | **0** | **1** | **1** | **1** | **1** | **0** | **0** | **1** | **1** | **1** | 
-| :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: |
-| **State** | A | A | B | C | C | D | A | B | C | D | B | B | B | C | D | B |
-| **Output R** | 0 | 0 | 0 | 0 | 0 | 1 | 0 | 0 | 0 | 1 | 0 | 0 | 0 | 0 | 1 | 0 |
+------------------------------------------------------------------------
+-- Entity declaration for traffic light controller
+------------------------------------------------------------------------
+entity smart_tlc is
+    port(
+        clk     : in  std_logic;
+        reset   : in  std_logic;
+        -- Traffic lights (RGB LEDs) for two directions
+        south_o : out std_logic_vector(3 - 1 downto 0);
+        west_o  : out std_logic_vector(3 - 1 downto 0)
+    );
+end entity smart_tlc;
 
-## Connection of RGB LEDs:
+------------------------------------------------------------------------
+-- Architecture declaration for traffic light controller
+------------------------------------------------------------------------
+architecture Behavioral of smart_tlc is
 
-| **RGB LED** | **Artix-7 pin names** | **Red** | **Yellow** | **Green** |
-| :-: | :-: | :-: | :-: | :-: |
-| LD16 | N15, M16, R12 | 1,0,0 | 1,1,0 | 0,1,0 |
-| LD17 | N16, R11, G14 | 1,0,0 | 1,1,0 | 0,1,0 |
+    -- Define the states
+    type   t_state is (STOP1, WEST_GO,  WEST_WAIT,
+                       STOP2, SOUTH_GO, SOUTH_WAIT);
+    -- Define the signal that uses different states
+    signal s_state  : t_state;
 
+    -- Internal clock enable
+    signal s_en     : std_logic;
+    -- Local delay counter
+    signal   s_cnt  : unsigned(5 - 1 downto 0);
 
-# 2. Traffic light controller
+    -- Specific values for local counter
+    constant c_DELAY_4SEC : unsigned(5 - 1 downto 0) := b"1_0000";
+    constant c_DELAY_2SEC : unsigned(5 - 1 downto 0) := b"0_1000";
+    constant c_DELAY_1SEC : unsigned(5 - 1 downto 0) := b"0_0100";
+    constant c_ZERO       : unsigned(5 - 1 downto 0) := b"0_0000";
 
-## State Diagram
+begin
 
-![alt text](Images/state_diagram.png)
+    --------------------------------------------------------------------
+    -- Instance (copy) of clock_enable entity generates an enable pulse
+    -- every 250 ms (4 Hz). Remember that the frequency of the clock 
+    -- signal is 100 MHz.
+    
+    -- JUST FOR SHORTER/FASTER SIMULATION
+    s_en <= '1';
+--    clk_en0 : entity work.clock_enable
+--        generic map(
+--            g_MAX =>        -- g_MAX = 250 ms / (1/100 MHz)
+--        )
+--        port map(
+--            clk   => clk,
+--            reset => reset,
+--            ce_o  => s_en
+--        );
 
-## sequential process:
-
-```vhdl
-p_traffic_fsm : process(clk)
+    --------------------------------------------------------------------
+    -- p_traffic_fsm:
+    -- The sequential process with synchronous reset and clock_enable 
+    -- entirely controls the s_state signal by CASE statement.
+    --------------------------------------------------------------------
+    p_traffic_fsm : process(clk)
     begin
         if rising_edge(clk) then
             if (reset = '1') then       -- Synchronous reset
@@ -107,12 +163,14 @@ p_traffic_fsm : process(clk)
             end if; -- Synchronous reset
         end if; -- Rising edge
     end process p_traffic_fsm;
-```
 
-## combinatorial process:
-
-```vhdl
-p_output_fsm : process(s_state)
+    --------------------------------------------------------------------
+    -- p_output_fsm:
+    -- The combinatorial process is sensitive to state changes, and sets
+    -- the output signals accordingly. This is an example of a Moore 
+    -- state machine because the output is set based on the active state.
+    --------------------------------------------------------------------
+    p_output_fsm : process(s_state)
     begin
         case s_state is
             when STOP1 =>
@@ -144,22 +202,5 @@ p_output_fsm : process(s_state)
                 west_o  <= "100";   -- Red
         end case;
     end process p_output_fsm;
-```
-## Screenshot:
 
-![alt text](Images/Graph1.PNG)
-
-# 3. Smart controller
-
-## State Table
-
-| **Current State** | **Direction South** | **Direction West** | **Delay** | **No cars 00** | **Car to west 01** | ** Car to south 10** | **11** |
-| :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: |
-
-| STOP1 | red | red | 1 sec | STOP | WEST_GO | SOUTH_GO | SOUTH_GO |
-| STOP2 | red | red | 1 sec | STOP | WEST_GO | SOUTH_GO | WEST_GO |
-| WEST_GO | red | green | 4 sec | WEST_GO | WEST_GO | WEST_WAIT | WEST_WAIT |
-| WEST_WAIT | red | yellow | 2 sec | STOP1 | STOP1 | STOP1 | STOP1 |
-| SOUTH_GO | green | red | 4 sec | SOUTH_GO | SOUTH_WAIT | SOUTH_GO | SOUTH_WAIT |
-| SOUTH_WAIT | yellow | red | 2 sec | STOP2 | STOP2 | STOP2 | STOP2 |
-
+end architecture Behavioral;
